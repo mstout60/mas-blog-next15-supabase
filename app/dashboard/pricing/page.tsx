c5c12db6e9c67b56a0d5e7b852ab558904fcdb02
mstout60 <mstout@eprospector.com>
2024-12-31 15:31:46 -0600

    Implement the Marketing Page

--- app/dashboard/pricing/page.tsx
+++ app/dashboard/pricing/page.tsx
@@ -1,6 +1,69 @@
+import { SubmitButton } from "@/app/components/dashboard/submit-buttons";
 import { PricingTable } from "@/app/components/shared/pricing";
+import prisma from "@/app/utils/db";
+import { requireUser } from "@/app/utils/require-user";
+import { stripe } from "@/app/utils/stripe";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { redirect } from "next/navigation";
 
-export default function PricingPage() {
+async function getData(userId: string) {
+  const data = await prisma.subscription.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      status: true,
+      User: {
+        select: {
+          customerId: true,
+        },
+      },
+    },
+  });
+
+  return data;
+}
+
+export default async function PricingPage() {
+  const user = await requireUser();
+  const data = await getData(user.id);
+
+  async function createCustomerPortal() {
+    "use server";
+
+    const session = await stripe.billingPortal.sessions.create({
+      customer: data?.User?.customerId as string,
+      return_url: "http://localhost:3000/dashboard",
+    });
+
+    return redirect(session.url);
+  }
+
+  if (data?.status === "active") {
+    return (
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Edit Subscription</CardTitle>
+          <CardDescription>
+            Click on the button below. This will allow for you to modify your
+            subcription and see your current invoices.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action={createCustomerPortal}>
+            <SubmitButton text="View Subscription Details" />
+          </form>
+        </CardContent>
+      </Card>
+    );
+  }
+
   return (
     <div>
       <PricingTable />
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,24 @@
-import { Button } from "@/components/ui/button";
-import {
-  RegisterLink,
-  LoginLink,
-  LogoutLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Hero } from "@/app/components/frontend/hero";
+import { Logos } from "@/app/components/frontend/Logos";
+import { Features } from "@/app/components/frontend/features";
+import { PricingTable } from "@/app/components/shared/pricing";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const { getUser } = getKindeServerSession();
   const session = await getUser();
 
+  if (session?.id) {
+    return redirect("/dashboard");
+  }
+
   return (
-    <div className="p-10">
-      <h1>Hello World</h1>
-      {session ? (
-        <LogoutLink>
-          <Button>Logout</Button>
-        </LogoutLink>
-      ) : (
-        <div>
-          <RegisterLink>
-            <Button>Register</Button>
-          </RegisterLink>
-          <LoginLink>
-            <Button>Login</Button>
-          </LoginLink>
-        </div>
-      )}
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
+      <Hero />
+      <Logos />
+      <Features />
+      <PricingTable />
     </div>
   );
 }
