f702202cd5aa90d2912779ac14df17fb7106f345
mstout60 <mstout@eprospector.com>
2024-12-31 17:53:34 -0600

    Changed production routing

--- app/actions.ts
+++ app/actions.ts
@@ -228,8 +228,14 @@ export async function CreateSubscription() {
       address: "auto",
       name: "auto",
     },
-    success_url: "http://localhost:3000/dashboard/payment/success",
-    cancel_url: "http://localhost:3000/dashboard/payment/cancelled",
+    success_url:
+      process.env.NODE_ENV === "production"
+        ? "https://mas-blog-next15-supabase.vercel.app/dashboard/payment/success"
+        : "http://localhost:3000/dashboard/payment/success",
+    cancel_url:
+      process.env.NODE_ENV === "production"
+        ? "https://mas-blog-next15-supabase.vercel.app/dashboard/payment/cancelled"
+        : "http://localhost:3000/dashboard/payment/cancelled",
     line_items: [
       {
         price: process.env.STRIPE_PRICE_ID,
